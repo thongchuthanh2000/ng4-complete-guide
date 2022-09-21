5dd0b9fc2392d88e642925330e59ff1d985dbd7c
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2022-09-22 02:01:40 +0700

    Change Subject

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipes.service';
 @Component({
   selector: 'app-recipes',
@@ -9,14 +8,10 @@ import { RecipeService } from './recipes.service';
 })
 export class RecipesComponent implements OnInit {
 
-  selectedRecipe!: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.selectedRecipe = recipe;
-    });
+
   }
 
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,3 +1,4 @@
+import { Subject } from 'rxjs';
 import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../share/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
@@ -5,8 +6,6 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-
-  recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
